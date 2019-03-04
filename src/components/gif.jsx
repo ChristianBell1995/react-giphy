237bf198401adea02c4e295a7fa4ab368f8a0d49
Christian Bell <christiantcbell@gmail.com>
2019-03-04 14:05:26 +0000

    one hour through React Basics lecture, just put gif componenet in

---
+++ src/components/gif.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  render() {
+    const src = ""
+
+    return(
+      <img src={src} alt="" className="gif"/>
+    )
+  }
+}
+
+export default Gif;
--- src/index.jsx
+++ src/index.jsx
@@ -2,48 +2,9 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
-
-// const Hello = (props) => {
-//   return (
-//     <div>
-//       Hello,
-//       {props.name}
-//       {props.surname}
-//     </div>
-//   );
-// };
-
-class Hello extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      clicked: false
-    }
-  }
-
-  handleClick = () => {
-    this.setState({
-      clicked: !this.state.clicked
-
-    })
-  }
-
-  render() {
-    // BUILD AND RETURN HTML
-    const { name, surname } = this.props;
-    return (
-      <div className={this.state.clicked ? 'clicked' : null} onClick={this.handleClick}>
-        Hello,
-        {name}
-        {surname}
-      </div>
-    )
-  }
-}
-
+import App from './components/app.jsx'
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="Christian" surname="Bell" />, root);
+  ReactDOM.render(<App />, root);
 }
